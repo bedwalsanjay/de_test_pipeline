5454e0eb80a1640673602655540d70b316f900e2
sanjay_bedwal <bedwalsanjay@gmail.com>
2023-10-31 13:29:51 +0530

    removed extra code

--- src/add.py
+++ src/add.py
@@ -3,12 +3,6 @@ import sys
 def add (a, b):
     return (a + b)
 
-def add1 (a, b):
-    return (a + b)
-
-def add2 (a, b):
-    return (a + b)
-
 def sub (a, b):
     return (a - b)
 
@@ -31,16 +25,6 @@ def calculate_cube(number):
     return number ** 3
 
 def calculate_factorial(number):
-    if number == 0:
-        return 1
-    else:
-        result = 1
-        for i in range(1, number + 1):
-            result *= i
-        return result
-
-
-def calculate_factorial1(number):
     if number == 0:
         return 1
     else:
