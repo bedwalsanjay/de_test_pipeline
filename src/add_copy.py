a2fb55ff201537ffe39f2176a77073e2fb642b1e
sanjay_bedwal <bedwalsanjay@gmail.com>
2023-11-02 13:04:56 +0530

    test

--- src/add_copy.py
+++ src/add_copy.py
@@ -49,3 +49,12 @@ def calculate_factorial1(number):
             result *= i
         return result
 
+
+def calculate_factorial2(number):
+    if number == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, number + 1):
+            result *= i
+        return result
